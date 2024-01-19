07eefbf8053f9c5878a0bcbd26570b1e438d864f
Hoa Pham <hoa.phamlethai@gmail.com>
2024-01-19 09:25:51 -0800

    fix telescope

--- lua/hoa/plugins-setup.lua
+++ lua/hoa/plugins-setup.lua
@@ -41,7 +41,7 @@ return packer.startup(function(use)
 
   -- fuzzy finder
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
