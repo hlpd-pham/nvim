fa83ebf180b6df30096e8a8550b5d61bb77a0f1a
Hoa Pham <hoa.phamlethai@gmail.com>
2024-01-22 08:15:31 -0800

    cleanup init file

--- lua/hoa/core/options.lua
+++ lua/hoa/core/options.lua
@@ -73,7 +73,19 @@ vim.cmd([[
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
 
-
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
 
 
 
