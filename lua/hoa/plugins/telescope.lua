1515903a34d844f0544eace86388ba6646315741
Hoa Pham <hoa.phamlethai@gmail.com>
2024-02-02 08:59:13 -0800

    fix some remaps for lsp

--- lua/hoa/plugins/telescope.lua
+++ lua/hoa/plugins/telescope.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: unused-function, unused-local
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- project files instead of find files
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
@@ -13,3 +14,27 @@ require('telescope').setup{
   }
 }
 
+-- stole from https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+local on_attach  = function(_, bufnr)
+  -- NOTE: Remember that lua is a real programming language, and as such it is possible
+  -- to define small helper and utility functions so you don't have to repeat yourself
+  -- many times.
+  --
+  -- In this case, we create a function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
+end
